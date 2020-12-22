380e720dff3dbe0dcce203c777b24c03cb781495
sandisan <sandicha@in.ibm.com>
2020-12-22 13:35:13 +0530

    Add files via upload

---
+++ src/main/java/com/demo/RequestModel.java
@@ -0,0 +1,36 @@
+package com.demo;
+
+public class RequestModel {
+	
+	private String host;
+	private String port;
+	private String uri;
+	private String log;
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public String getLog() {
+		return log;
+	}
+	public void setLog(String log) {
+		this.log = log;
+	}
+	
+	
+
+}
