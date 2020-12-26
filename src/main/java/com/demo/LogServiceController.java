a187be1723f8cec6494525a8f29b350ba5b1a519
sandisan <sandicha@in.ibm.com>
2020-12-26 23:36:00 +0530

    Update LogServiceController.java

--- src/main/java/com/demo/LogServiceController.java
+++ src/main/java/com/demo/LogServiceController.java
@@ -13,6 +13,8 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
