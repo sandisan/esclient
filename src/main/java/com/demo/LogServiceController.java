8d3d2818ffbf6b66c01c26cc875523c55d591cd3
sandisan <sandicha@in.ibm.com>
2020-12-26 22:27:27 +0530

    Update LogServiceController.java

--- src/main/java/com/demo/LogServiceController.java
+++ src/main/java/com/demo/LogServiceController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -29,7 +33,15 @@ public class LogServiceController {
 	@GetMapping(value="/get")
 	public String getLogs() {
 		 try {
-				RestTemplate template = new RestTemplate();
+		     CloseableHttpClient httpClient
+                = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+				HttpComponentsClientHttpRequestFactory requestFactory
+						= new HttpComponentsClientHttpRequestFactory();
+				requestFactory.setHttpClient(httpClient);
+				RestTemplate restTemplate = new RestTemplate(requestFactory);
+				//RestTemplate template = new RestTemplate();
 				String url = "https://elasticsearch.ibm-common-services.svc:9200/_cluster/health?wait_for_status=yellow&timeout=50s&pretty";
 				ResponseEntity<String> response = template.getForEntity(url, String.class);
 				
